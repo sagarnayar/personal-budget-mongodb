e3e479c94852e4a93e56c747f40353107098ac6a
sagar <sagarnayar@gmail.com>
2023-10-13 22:26:09 -0400

    Followed MongoDb Instructions

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // "mongodb://127.0.0.1:27017";
 const mongoose = require("mongoose");
-const budgetModel = require("./models/budgetData");
+const budgetModel = require("./Datamodule/budgetData");
 const bodyParser = require("body-parser");
 const express = require("express");
 const cors = require("cors");
